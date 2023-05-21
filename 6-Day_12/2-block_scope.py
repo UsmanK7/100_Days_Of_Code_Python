f93c2466f3c3c1e578f37b332d4e862f3790be52
Muhammad Usman Khan <95441787+UsmanK7@users.noreply.github.com>
2023-05-21 17:07:03 +0500

    Add files via upload

---
+++ 6-Day_12/2-block_scope.py
@@ -0,0 +1,10 @@
+# unlike in java and c++, all the blocks like if, while for all these blocks of code dont count as
+# local scope
+
+level=3
+enemies=["skeleton","beast","zombie"]
+
+if level<5:
+    new_enemy=enemies[1]
+
+print(new_enemy)
